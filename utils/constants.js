60db8b5806ba15a392625b896a9520ad81caa348
Md. Masudul Kabir <masud.ncse@gmail.com>
2023-03-16 18:01:33 +0600

    modified

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
-export const BACKEND_URL = "http://api.themetaversecats.com";
+export const BACKEND_URL = "https://api.themetaversecats.com";
 // export const BACKEND_URL = "http://localhost:8000";
 
 export const TINYMCE_API_KEY = "ghoynl9ijzzptuohk7b7u9arnfzx82vvt8720ei40qkxr8ey";
